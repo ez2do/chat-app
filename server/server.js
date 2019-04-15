958ede992938bc040da77e51e35b8f534862be60
TuanAnh <tuananh2431998@gmail.com>
2019-04-15 09:49:41 +0700

    Still fix problem of start command

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 console.log(publicPath);
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static(publicPath));
