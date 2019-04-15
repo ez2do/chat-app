20133d7dbad773c8297850624300f53cffa41fce
TuanAnh <tuananh2431998@gmail.com>
2019-04-15 09:30:27 +0700

    change start command

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+console.log(publicPath);
+var port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static(publicPath));
